c5a20e79a0683837746927a8687724c1b01dbeb2
Viesturs Teivāns <vteivans@gmail.com>
2021-04-24 22:10:42 +0300

    ct. 22. apr. 2021
    - Elementu saraksta apskatīšana ar ciklu
    - Pop-up bildes elementa izveide ar JS
    - Elementa pievienošana lapai ar JS
    - Bildes atvēršana Pop-up pēc klikšķa
    - Pop-up elementa formatēšana

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log('JavaScript strādā!');
+let galerijaBildeElems = document.querySelectorAll('.galerija__bilde');
+
+console.log(galerijaBildeElems);
+
+let bildePopUp = new Image();
+bildePopUp.src = 'https://source.unsplash.com/CG6Gd__QIOY/640x472';
+bildePopUp.classList.add('galerija__bilde--popup');
+document.body.appendChild(bildePopUp);
+
+for (let galerijaBilde of galerijaBildeElems) {
+
+  galerijaBilde.addEventListener('click', function (event) {
+    // nesekojam linkam
+    event.preventDefault();
+
+    // Parādām to bildi uz kuras uzspiedām
+    bildePopUp.src = event.target.href;
+  });
+}
